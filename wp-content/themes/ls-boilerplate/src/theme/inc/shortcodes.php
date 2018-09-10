efe795b8b8ac95f379d875e99df002bb62592eda
Josh Jenkins <jsjenkins@gmail.com>
2018-09-10 00:25:32 -0500

    Update button shortcode

--- wp-content/themes/ls-boilerplate/src/theme/inc/shortcodes.php
+++ wp-content/themes/ls-boilerplate/src/theme/inc/shortcodes.php
@@ -4,16 +4,16 @@
 function button_shortcode($atts, $content = null) {
     extract( shortcode_atts( array(
         'url' => '#',
-        'tab' => false,
+        '_blank' => false,
         'cta' => false
     ), $atts ) );
     if($cta) {
         $alert_class = ' secondary';
     }
-    if($tab) {
+    if($_blank) {
         $new_window = ' target="_blank"';
     }
-    return '<a href="'.$url.'" style="margin-top: 10px;" class="button small'.$alert_class.'"'.$new_window.'>'.$content.'</a>';
+    return '<a href="'.$url.'" class="button '.$alert_class.'"'.$new_window.'>'.$content.'</a>';
 }
 
 add_shortcode('button', 'button_shortcode');
