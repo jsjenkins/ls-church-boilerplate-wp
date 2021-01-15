17e399425c68b5ab1116b4ec425fd286d234f6cb
Josh Jenkins <jsjenkins@gmail.com>
2021-01-15 11:10:53 -0600

    Lighten JS

--- wp-content/themes/ls-boilerplate/src/assets/js/app.js
+++ wp-content/themes/ls-boilerplate/src/assets/js/app.js
@@ -7,7 +7,7 @@ require('foundation-sites');
 
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
-//import './lib/foundation-explicit-pieces';
+import './lib/foundation-explicit-pieces';
 
 require('./lib/jquery.fitVids.js');
 require('./lib/jquery.matchHeight.js');
